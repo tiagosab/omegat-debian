34cf86adc52584203815455a1163717ae95737f7
Tiago Saboga <tiagosaboga@gmail.com>
2008-02-23 19:13:57 +0000

    omegat (1.7.3.update1+dfsg0-1) unstable; urgency=low
    * Add Vcs-Svn and Vcs-Browser fields.
    * New upstream release.
    * Change numbering scheme to accommodate update releases and
      rewrite watch file accordingly.
    * Set Debian Java Team as maintainer and myself as uploader.
    * Remove get-orig-source from debian/rules; use uscan instead.
    * repack-source.sh deletes temporary files and dirs.
    
    -- Tiago Saboga <tiagosaboga@gmail.com>  Sat, 23 Feb 2008 15:41:00 -0300
    
    git-svn-id: svn+ssh://svn.debian.org/svn/pkg-java/trunk/omegat@5865 d4c0bf3d-bfd5-0310-91ca-af900d1bad9e

--- debian/repack-source.sh
+++ debian/repack-source.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ $# -ne 3 ]
 then
@@ -15,12 +15,19 @@ BASEDIR=$TMPDIR/omegat-${VERSION}.dfsg0
 mkdir "$BASEDIR"
 unzip -d "$BASEDIR" "$FILENAME"
 
+# Remove third-party libraries
 rm -r "$BASEDIR"/lib
-rm "$BASEDIR"/release/win32-specific/*exe 
-# find "$BASEDIR" -type f -exec file {} \; | grep "CRLF, CR" | sed 's/:.*//' | xargs fromdos 
-# find "$BASEDIR" -type f -exec file {} \; | grep CRLF | sed 's/:.*//' | xargs fromdos
+
+# Remove win32 executables
+rm "$BASEDIR"/release/win32-specific/*exe
+
+# Repack
 GZIP=-9 tar -C "$TMPDIR" -czf ../omegat_${VERSION}.dfsg0.orig.tar.gz "omegat-${VERSION}.dfsg0"
+
+# Clean temporary files
 rm -rf "$TMPDIR"
+rm -rf "$BASEDIR"
+rm -f "$FILENAME"
 
 if [ $1 = --upstream-version ] ;
 then
