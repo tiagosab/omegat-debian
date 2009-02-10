ed1959c15de5e33ed641912634d2fff1862c2f0a
Tiago Saboga <tiagosaboga@gmail.com>
2009-02-10 23:54:10 +0000

    omegat (1.8.1.2+dfsg-1) unstable; urgency=low
      [ Tiago Saboga ]
      * New upstream version.
      * Move to main.
      * Return to standard version number.
      * debian/repack-source.sh
        - Remove hunspell libraries.
      * Bump policy version (no changes needed).
      * Bump debhelper compatibility level.
      * Ship our own version of shell wrapper, instead of patching upstream.
      * debian/copyright - fix lintian warnings:
        - Point to GPL-2 instead of GPL.
        - Replace (C) by Copyright.
      * debian/rules
        - Install upstream changelog.
    
      [ Onkar Shinde <onkarshinde@ubuntu.com> ]
      * debian/control
        - Use openjdk-6-jdk as build dependency.
        - Use 'openjdk-6-jre | sun-java5-jre | sun-java6-jre' as runtime
          dependency.
      * debian/rules
        - Set JAVA_HOME corresponding to openjdk-6-jdk.
      * debian/ant.properties
        - Set default source and target values to 1.4.
      * debian/watch
        - Fix expression to match only release versions. Do not directly use
          specific sourceforge mirror.
    
     -- Tiago Saboga <tiagosaboga@gmail.com>  Tue, 10 Feb 2009 16:05:37 -0200
    
    
    git-svn-id: svn+ssh://svn.debian.org/svn/pkg-java/trunk/omegat@7951 d4c0bf3d-bfd5-0310-91ca-af900d1bad9e

---
+++ debian/omegat.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+JAVA_OPTIONS='-Djava.library.path=/usr/lib/jni'
+
+if [ -x /usr/lib/jvm/java-6-openjdk/bin/java ]
+    then JAVABIN=/usr/lib/jvm/java-6-openjdk/bin/java
+elif [ -x /usr/lib/jvm/java-6-sun/bin/java ]
+    then JAVABIN=/usr/lib/jvm/java-6-sun/bin/java
+elif [ -x /usr/lib/jvm/java-1.5.0-sun/bin/java ]
+    then JAVABIN=/usr/lib/jvm/java-1.5.0-sun/bin/java
+else
+    echo "Suitable java binary not found."
+    exit 1
+fi
+
+exec $JAVABIN $JAVA_OPTIONS -jar /usr/share/java/OmegaT.jar $*
--- debian/repack-source.sh
+++ debian/repack-source.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 if [ $# -ne 3 ]
 then
     echo "Usage: $0 option version filename"
@@ -21,6 +23,9 @@ rm -r "$BASEDIR"/lib
 # Remove win32 executables
 rm "$BASEDIR"/release/win32-specific/*exe
 
+# Remove hunspell libraries
+rm -r "$BASEDIR"/native
+
 # Repack
 GZIP=-9 tar -C "$TMPDIR" -czf ../omegat_${VERSION}+dfsg.orig.tar.gz "omegat-${VERSION}+dfsg"
 
@@ -31,5 +36,5 @@ rm -f "$FILENAME"
 
 if [ $1 = --upstream-version ] ;
 then
-    uupdate --upstream-version $2 $3
+    uupdate --upstream-version $2 "omegat_${VERSION}+dfsg.orig.tar.gz"
 fi
